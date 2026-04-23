7c83cb9f5e1b440287c8c3501f785e5a52273250
longfangsong <longfangsong@icloud.com>
2026-04-24 00:01:28 +0200

    Use file access method which is compatable with cloudflare

--- src/agents/agentLoader.ts
+++ src/agents/agentLoader.ts
@@ -1,15 +1,15 @@
 import type { Template } from "../model/agent";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 
 export async function loadTemplates(configDir: string): Promise<Template[]> {
     const templates = [];
     
-    const files = fs.readdirSync(configDir).filter(f => f.endsWith(".json"));
+    const files = (await fs.readdir(configDir)).filter(f => f.endsWith(".json"));
     
     for (const file of files) {
         const filePath = path.join(configDir, file);
-        const content = fs.readFileSync(filePath, "utf-8");
+        const content = await fs.readFile(filePath, "utf-8");
         const template = JSON.parse(content) as Template;
         templates.push(template);
     }
