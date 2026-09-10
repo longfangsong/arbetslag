262a6af1700484cc2836017c7593506d15a39089
longfangsong <longfangsong@icloud.com>
2026-09-10 23:44:19 +0200

    Update pined messages

--- apps/telegram-bot/prompt/pin.ts
+++ apps/telegram-bot/prompt/pin.ts
@@ -17,22 +17,22 @@ export interface Pin {
 export const PINS: Pin[] = [
 	{
 		id: "bad_cloud_llm",
-		messageId: 136606,
+		messageId: 166544,
 		description: "当用户发送 批评云 LLM 提供商 服务质量 （如 服务挂掉、降智、偷偷换低级模型、错误触发安全护栏 等） 的信息 时引用",
 	},
 	{
 		id: "financial_joke",
-		messageId: 116320,
+		messageId: 166547,
 		description: "当用户发送 人类 在金融领域 不理性的行为 （如 名字相关 实际业务完全无关 的 股票 因为新闻暴涨暴跌） 的信息 时引用",
 	},
 	{
 		id: "human_evil",
-		messageId: 109228,
+		messageId: 166540,
 		description: "当用户发送 人类劣根性 （比如 不择手段获取利益和权力、政府和大企业做出破坏普通人权利的决定 等） 相关的信息 时引用",
 	},
 	{
 		id: "politician_lie",
-		messageId: 87951,
+		messageId: 166550,
 		description: "当用户发送 政治家撒谎、前后言论不一致、言行不一 的信息 时引用",
 	}
 ];
